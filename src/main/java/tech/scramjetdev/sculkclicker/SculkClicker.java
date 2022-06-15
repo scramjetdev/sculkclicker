ed446dd27658e9f427c30393547d8afff94578f3
Hygon <hygon806@gmail.com>
2022-06-15 16:35:16 +0200

    Add Log4J

--- src/main/java/tech/scramjetdev/sculkclicker/SculkClicker.java
+++ src/main/java/tech/scramjetdev/sculkclicker/SculkClicker.java
@@ -2,22 +2,26 @@ package tech.scramjetdev.sculkclicker;
 
 import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 
 public class SculkClicker {
+    private static final Logger LOGGER = LogManager.getLogger(SculkClicker.class);
+
     private ArrayList<Long> rightClicks = new ArrayList<>();
     private ArrayList<Long> leftClicks = new ArrayList<>();
 
     public SculkClicker() {
-        System.out.println("Initializing SculkClicker...");
+        LOGGER.info("Initializing SculkClicker...");
 
         try {
             GlobalScreen.registerNativeHook();
         } catch (NativeHookException exception) {
-            System.err.println("A problem happened whilst registering native hooks.");
-            exception.printStackTrace();
+            LOGGER.error("A problem happened whilst registering native hooks.", exception);
             System.exit(1);
+            return;
         }
 
         GlobalScreen.addNativeMouseListener(new MouseListener(this));
